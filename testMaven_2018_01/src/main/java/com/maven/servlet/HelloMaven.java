9c10491dd7921e93dee677a4485d7bc72b0961c7
lujian <apple60588@vip.qq.com>
2018-06-10 17:29:50 +0800

    1

--- testMaven_2018_01/src/main/java/com/maven/servlet/HelloMaven.java
+++ testMaven_2018_01/src/main/java/com/maven/servlet/HelloMaven.java
@@ -16,7 +16,7 @@ public class HelloMaven extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("hello.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
