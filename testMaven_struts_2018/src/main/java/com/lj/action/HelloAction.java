3f91c2c30d66bd278ab49db5dc31c7ea007f3cc9
lujian <apple60588@vip.qq.com>
2018-06-14 19:20:09 +0800

    1

--- testMaven_struts_2018/src/main/java/com/lj/action/HelloAction.java
+++ testMaven_struts_2018/src/main/java/com/lj/action/HelloAction.java
@@ -5,6 +5,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class HelloAction extends ActionSupport{
 	
 	public String hello() {
+		System.out.println("进入action");
 		return SUCCESS;
 	}
 }
