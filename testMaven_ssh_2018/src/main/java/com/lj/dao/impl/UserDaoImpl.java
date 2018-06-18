53e3d7a83eb591e1d04422b65ce768a976ff37b9
lujian <apple60588@vip.qq.com>
2018-06-18 16:01:58 +0800

    1

--- testMaven_ssh_2018/src/main/java/com/lj/dao/impl/UserDaoImpl.java
+++ testMaven_ssh_2018/src/main/java/com/lj/dao/impl/UserDaoImpl.java
@@ -1,5 +1,11 @@
 package com.lj.dao.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import com.lj.dao.UserDao;
@@ -9,7 +15,13 @@ public class UserDaoImpl extends HibernateDaoSupport implements UserDao{
 
 	@Override
 	public User selectUserById(int id) {
-		return getHibernateTemplate().get(User.class, id);
+		Session session = getHibernateTemplate().getSessionFactory().getCurrentSession();
+		Query query = session.createQuery("from User where id = :id");
+		query.setParameter("id", id);
+		List<User> users = (List<User>) query.list();
+		//List<User> users = (List<User>) getHibernateTemplate().find("from User where id = ?", id);
+		//return getHibernateTemplate().get(User.class, id);
+		return users.get(0);
 	}
 
 }
